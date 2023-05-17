cc7766d8f46b991d2c61830cb72845b85af19be7
Polina Fomicheva <116427492+polinaburger@users.noreply.github.com>
2023-05-17 16:56:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,24 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+// поместите сюда свой код
+BST<std::string> bst;
+  std::ifstream file(filename);
+  std::string word = "";
+  if (!file.is_open()) {
+    throw std::string("Error! Can not open file!");
+  }
+  while (!file.eof()) {
+    word = "";
+    while (true) {
+      char dop = file.get();
+      if ((dop >= 65 && dop <= 90) || (dop >= 97 && dop <= 122)) {
+        word += tolower(dop);
+      } else {
+        break;
+      }
+    }
+    bst.Add(word);
+  }
+  return bst;
 }
